4d6a8761d243ed0506dec047cb2f922cafb354a1
Jakub Oboza <jakub.oboza@gmail.com>
2011-11-19 22:19:52 +0000

    completed refactor of config files handling

--- myocamlbuild.ml
+++
@@ -1,8 +0,0 @@
-open Ocamlbuild_plugin;;
-open Command;;
-
-dispatch begin function
-| After_rules ->
-  flag ["ocaml"; "native"; "link"; "program"] & A"config_file.cmx";
-| _ -> ()
-end;;
--- src/config.ml
+++ src/config.ml
@@ -17,23 +17,23 @@
   --------------------------------------------------------------------------*)
 
 open Yojson.Safe
-open Config_file
+
+exception Wrong_type of string
+let string = function `String s -> s | _ -> raise (Wrong_type "string")
+let int = function `Int i -> i | _ -> raise (Wrong_type "int")
+
 module NewClient = struct
 
-  exception Wrong_type of string
 
+
+  let config_filename = "/.indigo.json"
   (* TODO add File.join / Unix.join *)
-  let config_file = (Unix.getpwuid (Unix.getuid ())).Unix.pw_dir ^ "/.indigo.json"
+  let config_file = (Unix.getpwuid (Unix.getuid ())).Unix.pw_dir ^ config_filename
   let config = from_file config_file
   let default_host = "danmey.org"
   let default_uname = "wojtek"
   let default_port = 1234
-  let default_profile = "main"
-
-  let int = function `Int i -> i | _ -> raise (Wrong_type "int")
-  let string = function `String s -> s | _ -> raise (Wrong_type "string")
-
- 
+  let default_profile = "main" 
 
   let read () = 
     let config = from_file config_file in
@@ -51,7 +51,21 @@ module NewClient = struct
            { LoginData.uname;
              LoginData.pass; }) } 
 
-  let write data = ()
+  let write { LoginData.host;
+	      LoginData.port;
+	      LoginData.login } =
+    let json = `Assoc (["host", `String host;
+              "port", `Int port;] @
+		 match login with
+		   | Some (LoginData.FullLogin 
+			     { LoginData.uname;
+			       LoginData.pass; }) ->
+		     ["uname", `String uname;
+		      "pass", `String pass]
+		   | Some (LoginData.Uname uname) ->
+		     ["uname", `String uname]
+		   | None -> []) in
+    to_file config_file json
 
   let with_profile f = let data = read () in 
                        let data = f data  in
@@ -62,83 +76,15 @@ module NewClient = struct
 end
 
 
-
-module Client = struct
-  let config_file =  "/.indigo.json"
-  let default_host = "danmey.org"
-  let default_uname = "wojtek"
-  let default_port = 1234
-  let default_profile = "main"
-  
-  let entries group profile =
-    let host_cp = new string_cp ~group [profile; "host"] default_host "Host name." in
-    let port_cp = new int_cp ~group [profile; "port"] default_port "Port." in
-    let uname_cp = new string_cp ~group [profile; "uname"] default_uname "User name." in
-    let pass_cp = new string_cp ~group [profile; "pass"] "" "Password." in
-    host_cp, port_cp, uname_cp, pass_cp
-
-  let setup () =
-    let group = new group in
-    let profile_cp = (new list_cp string_wrappers ~group ["profiles"] [default_profile] "List of profiles.") in
-    group#read config_file;
-    let profile, (host_cp, port_cp, uname_cp, pass_cp)
-      = match profile_cp # get with
-        | profile :: _ -> 
-          profile, entries group profile
-        | [] ->
-          let (host_cp, port_cp, uname_cp, pass_cp as entries) = entries group default_profile in
-          group # add host_cp;
-          group # add port_cp; 
-          group # add uname_cp;
-          group # add pass_cp;
-          default_profile, entries in
-    group#read config_file;
-    group, profile, host_cp, port_cp, uname_cp, pass_cp
-
-  let read () =
-    let group, profile, host_cp, port_cp, uname_cp, pass_cp = setup () in
-    { LoginData.host = host_cp # get;
-      LoginData.port = port_cp # get;
-      LoginData.login = Some 
-        (LoginData.FullLogin 
-           { LoginData.uname = uname_cp # get;
-             LoginData.pass = pass_cp # get; }) }
-      
-  let write data =
-    let group, profile, host_cp, port_cp, uname_cp, pass_cp = setup () in
-    host_cp # set data.LoginData.host;
-    port_cp # set data.LoginData.port;
-    begin match data.LoginData.login with
-      | None -> ()
-      | Some login ->
-        match login with
-          |  (LoginData.FullLogin 
-                { LoginData.uname = uname;
-                  LoginData.pass = pass }) ->
-            uname_cp # set uname;
-            pass_cp # set pass;
-          | LoginData.Uname uname ->
-            uname_cp # set uname end;
-    group # write config_file
-      
-
-  let with_profile f = let data = read () in 
-                       let data = f data in
-                       match data with 
-                         | Some data -> write data; Some data 
-                         | None -> data
-end
-
-
 module Server = struct
-  let config_file = (Unix.getpwuid (Unix.getuid ())).Unix.pw_dir ^ "/.indigo-server.ml"
+
+  let config_filename = "/.indigo-server.json"
+  let config_file = (Unix.getpwuid (Unix.getuid ())).Unix.pw_dir ^ config_filename
+
   let read () =
-    let group = new group in
-    let auth_list_cp = 
-      new list_cp (tuple2_wrappers string_wrappers string_wrappers)
-        ~group ["passwd"] ["admin", "admin"] "List of users password." 
-    in
-    group # read config_file;
-    auth_list_cp # get
-      
+    let config = from_file config_file in
+      match config with 
+        | `Assoc lst -> (List.map (fun (name, pass)-> name, string pass) lst)       | _ -> []
+  
 end
+
