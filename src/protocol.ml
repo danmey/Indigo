5c46f7b1ceb5ab6a6399ee46cebc61f69b382c27
Jakub Oboza <jakub.oboza@gmail.com>
2011-11-19 17:45:39 +0000

    Added more patterns to client side logger

--- src/protocol.ml
+++ src/protocol.ml
@@ -41,17 +41,35 @@ module Log = struct
   let log = function
     | Server cmd -> begin
       match cmd with
-	| Disconnect str -> LOG "Client '%s' disconnected" str LEVEL DEBUG
-	| RequestLogin (username, pass) -> LOG "RequestLogin from %s with password %s" username pass LEVEL DEBUG
-        | RequestUserList -> LOG "RequestUserList" LEVEL DEBUG
-        | Quit user -> LOG "User '%s' has quit" user LEVEL DEBUG
-        | Kick user -> LOG "User '%s' was kicked from server" user LEVEL DEBUG
-	| _ -> ()
+	| Disconnect str -> LOG "> Client '%s' disconnected" str LEVEL DEBUG
+	| RequestLogin (username, pass) -> LOG "> RequestLogin from %s with password %s" username pass LEVEL DEBUG
+        | RequestUserList -> LOG "> RequestUserList" LEVEL DEBUG
+        | Quit user -> LOG "> User '%s' has quit" user LEVEL DEBUG
+        | Kick user -> LOG "> User '%s' was kicked from server" user LEVEL DEBUG
+	| _ -> LOG "> Unknown message" LEVEL DEBUG
     end
-    | _ -> ()
+    | Client cmd -> begin
+      match cmd with
+        | BadPassword str -> LOG "< BadPassword for user '%s'" str LEVEL DEBUG
+        | Login user -> LOG "< Login for user '%s'" user LEVEL DEBUG
+        | MoveElement (what, (x, y)) -> LOG "Moved Element %s to (%d, %d)" what x y LEVEL DEBUG
+        | BadUser user -> LOG "< Bad user '%s'" user LEVEL DEBUG
+        | NewUser user -> LOG "< New user '%s'" user LEVEL DEBUG
+        | UserAlreadyLoggedIn user -> LOG "< User '%s' already logged in" user LEVEL DEBUG 
+        | KickUser user -> LOG "< User '%s' has been kicked" user LEVEL DEBUG
+        (* TODO | UserList of string list *)
+        | _ -> LOG "< Unknow message" LEVEL DEBUG
+    end
 
   let input_value ch =
     lwt v = Lwt_chan.input_value ch in
     log v;
     Lwt.return v
+
+  let output_value ch v =
+    log v;
+    Lwt_chan.output_value ch v
+
 end
+
+
