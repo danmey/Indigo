57cff217a155e6c398f854548a5fe57ddd57bbad
Wojciech Meyer <wojciech.meyer@gmail.com>
2011-11-28 00:22:30 +0000

    Curved notes

--- src/mainFrame.ml
+++ src/mainFrame.ml
@@ -54,11 +54,25 @@ module GtkBackend = struct
         (* gc # string ~x ~y ~font:(Lazy.force font) text; *)
         ())
         
+    let draw_curved_rectangle cr ~x ~y ~width ~height =
+	Cairo.save cr;
+	Cairo.move_to cr ~x ~y:(y+.height /. 2.);
+	Cairo.curve_to cr ~x1:x ~y1:y ~x2:x ~y2:y ~x3:(x +. width /. 2.) ~y3:y;
+	Cairo.curve_to cr ~x1:(x +. width) ~y1:y ~x2:(x +. width) ~y2:y ~x3:(x +. width) ~y3:(y +. height /. 2.);
+	Cairo.curve_to cr ~x1:(x+. width) ~y1:(y+. height) ~x2:(x +. width) ~y2:(y +. height) ~x3:(x +. width /. 2.) ~y3:(y +. height);
+	Cairo.curve_to cr ~x1:x ~y1:(y +. height) ~x2:x ~y2:(y +. height) ~x3:x ~y3:(y +. height /. 2.);
+	Cairo.restore cr
+ 
+
     let rectangle ~pos:(x,y) ~size:(width,height) = 
       let x,y,width, height = float x, float y, float width, float height in
       with_canvas (fun gc ->
         let cr = Cairo_lablgtk.create (gc)#pixmap in
-        Cairo.rectangle cr ~x ~y ~width ~height;
+        let pat = (Cairo.Pattern.create_linear ~x0:x ~y0:y ~x1:(x+.width) ~y1:(y+.height)) in
+        Cairo.Pattern.add_color_stop_rgb pat ~off:0.0 ~red:0.6 ~green:0.6 ~blue:0.7;
+        Cairo.Pattern.add_color_stop_rgb pat ~off:1.0 ~red:0.6 ~green:0.6 ~blue:1.0;
+        Cairo.set_source cr pat;
+        draw_curved_rectangle cr ~x ~y ~width ~height;        (* Cairo.rectangle cr ~x ~y ~width ~height; *)
         Cairo.fill cr;
       )
 
