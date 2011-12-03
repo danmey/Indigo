49f929f0ccfe98c31f2c9be43b3aa67b6a2f50ef
Wojciech Meyer <wojciech.meyer@gmail.com>
2011-12-03 01:44:17 +0000

    Make Rect and Pos float based.

--- src/pos.ml
+++ src/pos.ml
@@ -16,15 +16,15 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
   --------------------------------------------------------------------------*)
 
-type t = (int*int)
+type t = (float*float)
 let fold2 f (x1,y1) (x2,y2) = (f x1 x2, f y1 y2)
-let sub = fold2 (-)
-let add = fold2 (+)
+let sub = fold2 (-.)
+let add = fold2 (+.)
 let abs (x,y) = abs x, abs y
 let x = fst
 let y = snd
 let set_x p ax = (ax, y p)
 let set_y p ay = (x p, ay)
-let add_x p p' = (x p + x p', y p)
+let add_x p p' = (x p +. x p', y p)
 let add_y p p' = (x p,y p + y p')
-let to_string (x,y) = Printf.sprintf "(%d, %d)" x y
+let to_string (x,y) = Printf.sprintf "(%f, %f)" x y
--- src/rect.ml
+++ src/rect.ml
@@ -16,41 +16,40 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
   --------------------------------------------------------------------------*)
 
-type t = {mutable x:int; mutable y:int; mutable w:int; mutable h:int}
+type t = {mutable x:float; mutable y:float; mutable w:float; mutable h:float}
 let rect (x,y) (w,h) = {x=x; y=y; w=w; h=h; }
 let size r = (r.w,r.h)
-let abs_dim r = (r.x, r.y),(r.x+r.w,r.y+r.h)
+let abs_dim r = (r.x, r.y),(r.x+.r.w,r.y+.r.h)
 let pos r = (r.x, r.y)
 let subr r1 r2 = 
   let p1 = pos r1 in 
   let p2 = pos r2 in 
   rect (Pos.sub p2 p1) (size r1)
 let set_pos r (x,y) = r.x <- x; r.y <- y
-let is_in r (x,y) = x >= r.x && x < r.x + r.w && y >= r.y && y < r.y + r.h
+let is_in r (x,y) = x >= r.x && x < r.x +. r.w && y >= r.y && y < r.y +. r.h
 let together r1 r2 =
   let x, y = max r1.x r2.x, max r1.y r2.y in
-  let x',y' = min (r1.x + r1.w) (r2.x + r2.w), min (r1.y + r1.h) (r2.y + r2.h) in
-  rect (x,y) (x'-x, y'-y)
-let pos_in r (x,y) = (x-r.x,y-r.y)
+  let x',y' = min (r1.x +. r1.w) (r2.x +. r2.w), min (r1.y +. r1.h) (r2.y +. r2.h) in
+  rect (x,y) (x'-.x, y'-.y)
+let pos_in r (x,y) = (x-.r.x,y-.r.y)
 let fold2 f r (x,y) = {r with x=f r.x x; y=f r.y y}
-let sub = fold2 (-)
-let by = fold2 (+)
-let by_h r (x,_) = { r with x=r.x+x}
-let by_v r (_,y) = { r with y=r.y+y}
-let o = rect (0,0) (0,0)
+let sub = fold2 (-.)
+let by = fold2 (+.)
+let by_h r (x,_) = { r with x=r.x+.x}
+let by_v r (_,y) = { r with y=r.y+.y}
+let o = rect (0.,0.) (0.,0.)
 let place_in r1 r2 = let p = pos r2 in by r1 p
-let border n r = rect (r.x + n / 2, r.y + n / 2) (r.w - n, r.h - n)
-let string_of_rect r = Printf.sprintf "Rect(%d,%d,%d,%d)" r.x r.y r.w r.h
+let border n r = rect (r.x +. n /.2., r.y +. n /.2.) (r.w -. n, r.h -. n)
 let lift4 f { x; y; w; h; } = f x, f y, f w, f h
 let lift22 f g  { x; y; w; h; } = f x, f y, g w,g h
 let coords { x; y; w; h } = x,y,w,h
-let coordsf { x; y; w; h } = float x, float y, float w, float h
-let to_string { x; y; w; h; } = Printf.sprintf "{ x=%d; y=%d; w=%d; h=%d }" x y w h
+let coordsf { x; y; w; h } = x, y, w, h
+let to_string { x; y; w; h; } = Printf.sprintf "{ x=%f; y=%f; w=%f; h=%f }" x y w h
 let shrink { x; y; w; h; } pixels = 
-  { x= x + pixels; 
-    y = y + pixels; 
-    w = w - 2 * pixels; 
-    h = h - 2 * pixels }
+  { x= x +. pixels; 
+    y = y +. pixels; 
+    w = w -.2.*. pixels; 
+    h = h -.2.*. pixels }
 let scale s ({w; h} as r) = { r with 
-  w = int_of_float (float w *. s); 
-  h = int_of_float (float h *. s); }
+  w = w *. s; 
+  h = h *. s; }
