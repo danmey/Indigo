85ada0c931f9dfe9f113a25050a239ea4b71cc97
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-12-06 21:31:37 +0000

    Fix events

--- ui/example/simple.ml
+++ ui/example/simple.ml
@@ -18,38 +18,47 @@ module Client = struct
         | _ -> None
         end
       | Some status' ->
-        if status' <> status then
-          begin
-            last_status := Some status;
-            let mouse_x, mouse_y = G.(status.mouse_x, status.mouse_y) in
-            let code = G.(int_of_char status.key) in
-            match G.(status.button, status.keypressed, status'.button, status'.keypressed) with
-            | true,_,false,_ -> Some (E.(MouseUp ((), (mouse_x, mouse_y))))
-            | false,_,true,_ -> Some (E.(MouseDown ((), (mouse_x, mouse_y))))
-            | _,false,_,true -> Some (E.(KeyDown ((), (mouse_x, mouse_y), code)))
-            | _,true,_,false -> Some (E.(KeyUp ((), (mouse_x, mouse_y), code)))
-            | _ -> None
-          end
-        else None
+        let mouse_x, mouse_y = G.(status.mouse_x, status.mouse_y) in
+        let code = G.(int_of_char status.key) in
+        let ev = match G.(status'.button, status'.keypressed, status.button, status.keypressed) with
+        | false,_,true,_ -> Some (E.(MouseDown ((), (mouse_x, mouse_y))))
+        | true,_,false,_ -> Some (E.(MouseUp ((), (mouse_x, mouse_y))))
+        | _,false,_,true -> Some (E.(KeyDown ((), (mouse_x, mouse_y), code)))
+        | _,true,_,false -> Some (E.(KeyUp ((), (mouse_x, mouse_y), code)))
+        | _ -> None
+        in
+        if ev <> None then last_status := Some status;
+        ev
 
   let is_end_session = function
   | E.KeyDown _ -> true
   | _ -> false
 
   let repaint_window ~x ~y ~width ~height =
-    G.set_color G.black;
-    G.draw_rect x y width height;
     G.set_color G.white;
-    G.draw_rect x y width height
+    G.fill_rect x y width height;
+    G.set_color G.black;
+    G.draw_rect (x+2) (y+2) (width-4) (height-4)
 
-  let on_event window =
-    ()
+  let on_event window = function
+  | E.MouseDown (_,(x,y)) ->
+    Manager.open_window ~x ~y ~w:100 ~h:100 "test"
+  | _ -> ()
+
+  let redraw_screen ~x ~y ~width ~height =
+    G.synchronize();
+    G.set_color G.black;
+    G.fill_rect x y width height
+
+  let screen_rect () =
+    (0,0), (G.size_x (),G.size_y ())
 end
 
 module UI = Ui.Make(Client)
 
 let () =
   G.open_graph "";
+  G.auto_synchronize false;
   M.open_screen "main";
   UI.event_loop ();
   G.close_graph ()
--- ui/ui.ml
+++ ui/ui.ml
@@ -3,7 +3,9 @@ module type CLIENT = sig
   val poll_event : unit -> Event.prim option
   val is_end_session : Event.window -> bool
   val repaint_window : x:int -> y:int -> width:int -> height:int -> unit
-  val on_event : Window.t option -> unit
+  val on_event : Window.t option -> Event.window -> unit
+  val redraw_screen : x:int -> y:int -> width:int -> height:int -> unit
+  val screen_rect : unit -> (int * int) * (int * int)
 end
 
 module Make(Client : CLIENT) = struct
@@ -11,17 +13,28 @@ module Make(Client : CLIENT) = struct
   let translate_event x = x
 
   let event_loop () =
+    let (x, y), (width, height) = Client.screen_rect () in
+    Client.redraw_screen ~x ~y ~width ~height;
     let rec loop () =
+      Thread.yield();
       match Client.poll_event () with
       | Some event ->
         let event = translate_event event in
-        List.iter (fun window -> Client.on_event (Some window))
+        List.iter (fun window -> Client.on_event (Some window) event)
           Manager.((current_screen ()).Screen.windows);
-
+        List.iter (fun window ->
+          Window.(Client.repaint_window
+                    ~x:window.rel_x
+                    ~y:window.rel_y
+                    ~width:window.width
+                    ~height:window.height))
+          Manager.((current_screen ()).Screen.windows);
+        Client.on_event None event;
+        let (x, y), (width, height) = Client.screen_rect () in
+        Client.redraw_screen ~x ~y ~width ~height;
         if not (Client.is_end_session event) then
           loop ()
-      | None ->
-        loop ()
+      | None -> loop ()
     in
     loop ()
 
